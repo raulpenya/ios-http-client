47304b8ddd4797ddea0e46fb8a0cd80bffe15652
rpenya <rpenya.mail@gmail.com>
2026-02-09 16:00:18 +0100

    fix RequestCombineTests

--- Tests/HTTPClientTests/RequestCombineTests.swift
+++ Tests/HTTPClientTests/RequestCombineTests.swift
@@ -1,6 +1,6 @@
 //
 //  RequestCombineTests.swift
-//  iOS-Generic-Datasource-Tests
+//  HTTPClientTests
 //
 //  Created by raulbot on 24/2/23.
 //
@@ -8,147 +8,178 @@
 import XCTest
 @testable import HTTPClient
 
-
+@MainActor
 final class RequestCombineTests: XCTestCase {
-
-    let dataSource = MockGenericNetworkingDataSource()
-    let session = MockSession()
-    var transformCalled = false
-    var response: DataSourceResponse?
-    var errorResponse: Error?
-    var urlRequest: URLRequest?
-    var resource: Resource<PersonsRemoteEntity, [Person]>?
-    
-    override func setUpWithError() throws {
-        urlRequest = try! MockApi.getRequest.asURLRequest()
-        resource = Resource<PersonsRemoteEntity, [Person]>(request: urlRequest!) { [weak self] persons in
-            self?.transformCalled = true
-            return persons.transformToDomain()
-        }
-        transformCalled = false
-        response = nil
-        errorResponse = nil
-    }
     
     func test_request_success() {
         //Given
+        let session = MockSession()
         session.response = .success
+        let urlRequest = try! MockApi.getRequest.asURLRequest()
+        let dataSource = NetworkingDataSource(session: session)
         let expectation = expectation(description: "test_request_success")
+        var localResponse: DataSourceResponse?
+        var localError: Error?
+        var localTransformCalled = false
+        let resource = Resource<PersonsRemoteEntity, [Person]>(
+            request: urlRequest
+        ) { persons in
+            localTransformCalled = true
+            return persons.transformToDomain()
+        }
+        
         //When
-        dataSource.request(with: session, resource: resource).sink { [weak self] completion in
-            print(completion)
-            switch completion {
-            case .failure(let error):
-                self?.response = .error
-                self?.errorResponse = error
-            case .finished:
-                self?.response = .success
-            }
-            expectation.fulfill()
-        } receiveValue: { _ in }.cancel()
-        waitForExpectations(timeout: 5, handler: nil)
-        //Then
-        XCTAssertEqual(response, .success)
-        XCTAssertTrue(transformCalled)
-        XCTAssertNil(errorResponse)
-    }
-    
-    func test_request_noresource_error() {
-        //Given
-        session.response = .success
-        let expectation = expectation(description: "test_request_noresource_error")
-        resource = nil
-        //When
-        dataSource.request(with: session, resource: resource).sink { [weak self] completion in
-            print(completion)
-            switch completion {
-            case .failure(let error):
-                self?.response = .error
-                self?.errorResponse = error
-            case .finished:
-                self?.response = .success
-            }
-            expectation.fulfill()
-        } receiveValue: { _ in }.cancel()
-        waitForExpectations(timeout: 5, handler: nil)
-        //Then
-        XCTAssertEqual(response, .error)
-        XCTAssertFalse(transformCalled)
-        XCTAssertNotNil(errorResponse)
-        XCTAssertNotEqual((errorResponse! as NSError).domain.description, String.getErrorResponse())
-        XCTAssertEqual((errorResponse! as! DataSourceErrors).localizedDescription, DataSourceErrors.requestException.localizedDescription)
-        XCTAssertEqual((errorResponse! as! DataSourceErrors).code, DataSourceErrors.requestException.code)
+        let cancellable = dataSource
+            .request(resource: resource)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        localResponse = .error
+                        localError = error
+                    case .finished:
+                        localResponse = .success
+                    }
+                    expectation.fulfill()
+                },
+                receiveValue: { _ in }
+            )
+        waitForExpectations(timeout: 5)
+        
+        //THen
+        XCTAssertEqual(localResponse, .success)
+        XCTAssertTrue(localTransformCalled)
+        XCTAssertNil(localError)
+        
+        _ = cancellable
     }
     
     func test_request_error() {
         //Given
+        let session = MockSession()
         session.response = .error
+        let urlRequest = try! MockApi.getRequest.asURLRequest()
+        let dataSource = NetworkingDataSource(session: session)
         let expectation = expectation(description: "test_request_error")
+        var localResponse: DataSourceResponse?
+        var localError: Error?
+        var localTransformCalled = false
+        let resource = Resource<PersonsRemoteEntity, [Person]>(
+            request: urlRequest
+        ) { persons in
+            localTransformCalled = true
+            return persons.transformToDomain()
+        }
+        
         //When
-        dataSource.request(with: session, resource: resource).sink { [weak self] completion in
-            print(completion)
-            switch completion {
-            case .failure(let error):
-                self?.response = .error
-                self?.errorResponse = error
-            case .finished:
-                self?.response = .success
-            }
-            expectation.fulfill()
-        } receiveValue: { _ in }.cancel()
-        waitForExpectations(timeout: 5, handler: nil)
-        //Then
-        XCTAssertEqual(response, .error)
-        XCTAssertFalse(transformCalled)
-        XCTAssertNotNil(errorResponse)
+        let cancellable = dataSource
+            .request(resource: resource)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        localResponse = .error
+                        localError = error
+                    case .finished:
+                        localResponse = .success
+                    }
+                    expectation.fulfill()
+                },
+                receiveValue: { _ in }
+            )
+        waitForExpectations(timeout: 5)
+        
+        //THen
+        XCTAssertEqual(localResponse, .error)
+        XCTAssertFalse(localTransformCalled)
+        XCTAssertNotNil(localError)
+        
+        _ = cancellable
     }
     
-    func test_request_handleResponse_error() {
+    func test_request_handleResponse_error() {        
         //Given
+        let session = MockSession()
         session.response = .errorHandleResponse
+        let urlRequest = try! MockApi.getRequest.asURLRequest()
+        let dataSource = NetworkingDataSource(session: session)
         let expectation = expectation(description: "test_request_handleResponse_error")
+        var localResponse: DataSourceResponse?
+        var localError: Error?
+        var localTransformCalled = false
+        let resource = Resource<PersonsRemoteEntity, [Person]>(
+            request: urlRequest
+        ) { persons in
+            localTransformCalled = true
+            return persons.transformToDomain()
+        }
+        
         //When
-        dataSource.request(with: session, resource: resource).sink { [weak self] completion in
-            print(completion)
-            switch completion {
-            case .failure(let error):
-                self?.response = .error
-                self?.errorResponse = error
-            case .finished:
-                self?.response = .success
-            }
-            expectation.fulfill()
-        } receiveValue: { _ in }.cancel()
-        waitForExpectations(timeout: 5, handler: nil)
-        //Then
-        XCTAssertEqual(response, .error)
-        XCTAssertFalse(transformCalled)
-        XCTAssertNotNil(errorResponse)
-        XCTAssertNotEqual((errorResponse! as NSError).domain.description, String.getErrorResponse())
-        XCTAssertEqual((errorResponse! as! DataSourceErrors).localizedDescription, DataSourceErrors.castHTTPURLResponseException.localizedDescription)
-        XCTAssertEqual((errorResponse! as! DataSourceErrors).code, DataSourceErrors.castHTTPURLResponseException.code)
+        let cancellable = dataSource
+            .request(resource: resource)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        localResponse = .error
+                        localError = error
+                    case .finished:
+                        localResponse = .success
+                    }
+                    expectation.fulfill()
+                },
+                receiveValue: { _ in }
+            )
+        waitForExpectations(timeout: 5)
+        
+        //THen
+        XCTAssertEqual(localResponse, .error)
+        XCTAssertFalse(localTransformCalled)
+        XCTAssertNotNil(localError)
+        
+        _ = cancellable
     }
     
-    func test_request_decode_error() {
+    func test_request_decode_error() {        
         //Given
+        let session = MockSession()
         session.response = .errorDecode
+        let urlRequest = try! MockApi.getRequest.asURLRequest()
+        let dataSource = NetworkingDataSource(session: session)
         let expectation = expectation(description: "test_request_decode_error")
+        var localResponse: DataSourceResponse?
+        var localError: Error?
+        var localTransformCalled = false
+        let resource = Resource<PersonsRemoteEntity, [Person]>(
+            request: urlRequest
+        ) { persons in
+            localTransformCalled = true
+            return persons.transformToDomain()
+        }
+        
         //When
-        dataSource.request(with: session, resource: resource).sink { [weak self] completion in
-            print(completion)
-            switch completion {
-            case .failure(let error):
-                self?.response = .error
-                self?.errorResponse = error
-            case .finished:
-                self?.response = .success
-            }
-            expectation.fulfill()
-        } receiveValue: { _ in }.cancel()
-        waitForExpectations(timeout: 5, handler: nil)
-        //Then
-        XCTAssertEqual(response, .error)
-        XCTAssertFalse(transformCalled)
-        XCTAssertNotNil(errorResponse)
+        let cancellable = dataSource
+            .request(resource: resource)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error):
+                        localResponse = .error
+                        localError = error
+                    case .finished:
+                        localResponse = .success
+                    }
+                    expectation.fulfill()
+                },
+                receiveValue: { _ in }
+            )
+        waitForExpectations(timeout: 5)
+        
+        //THen
+        XCTAssertEqual(localResponse, .error)
+        XCTAssertFalse(localTransformCalled)
+        XCTAssertNotNil(localError)
+        
+        _ = cancellable
     }
 }
